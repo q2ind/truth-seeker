233dd384ca9aa56b85da7e12cd2ae7c22f9eb58f
Gary Chen <gary.chen@oath.com>
2018-12-21 10:52:11 -0500

    Removed commented lines

--- scripts/bash/link.sh
+++ scripts/bash/link.sh
@@ -13,69 +13,9 @@ notes_dir=/Users/garychen/Documents/mystuff/notes
 
 # only link directory
 ln -s ${docs_dir} $dest_dir
-#ln -s ${jira_dir} $dest_dir
 ln -s ${notes_dir} $dest_dir
-#ln -s ${jira_dir}/jira-on-going $dest_dir
 ln -s ${jira_dir} $dest_dir
 
-#echo "Link to work-related docs and directories..."
-#docs=(
-#"Display-aggregator-control-panels"
-#"Frequent-viewed-Docs"
-#"Reporting-UAT-and-Other-Stuff"
-#"Rpt-portal-migration-to-AWS"
-#"Upgrade-mac-os-to-High-Sierra"
-#"Yamplus-traffic-api-dev-notes"
-#"aolp-prd-data-encryption"
-#"aws-host-info"
-#"avata-info"
-#"aws-encryption-task"
-#"display_aggregator_URLs"
-#"myStuffToDo"
-#"new-relic-url"
-##"redis-master-key-info"
-#"toDo"
-#"things-to-learn"
-#"yahoo-hosts-access"
-#"yahoo-onboarding-checklist"
-#"yahoo-permanent-id-change"
-#"yahoo-api-development-guide"
-#"yahoo-tech"
-#"yamplus_api_build_instructions"
-#)
-
-## link documents in docs directory
-#for doc in "${docs[@]}"; do
-#    #echo "docs:${docs_dir}/${doc}"
-#    ln -s ${docs_dir}/${doc} $dest_dir
-#done
-
-#jira_docs=(
-#"vertica-test-notes"
-#"mysql-aws-tests"
-#"vertica-aws-tests"
-#)
-## link documents in jira_docs directory
-#for doc in "${jira_docs[@]}"; do
-#    #echo "jira_docs:${jira_dir}/${doc}"
-#    ln -s ${jira_dir}/${doc} $dest_dir
-#done
-
-#notes_docs=(
-#"aws_notes"
-#"java_notes"
-#"jenkins_notes"
-#"python_notes"
-#"scala_notes"
-#"seeker_notes"
-#)
-## link documents in notes_docs directory
-#for doc in "${notes_docs[@]}"; do
-#    #echo "notes_docs:${notes_dir}/${doc}"
-#    ln -s ${notes_dir}/${doc} $dest_dir
-#done
-#echo "== End =="
-#
 
 ## copy tasks and update start/end dates
 taskfile=$dest_dir/tasks
@@ -133,8 +73,10 @@ fi
 #out=$?
 #echo $out
 
+## link docs
+ln -s docs/myStuffToDo $dest_dir
 ## link reporting-portal related tasks (as of 11/01/2018)
-ln -s notes/reporting-b2b-upgrade $dest_dir
+#ln -s notes/reporting-b2b-upgrade $dest_dir
 ## 
 ## link kafka related notes (06/18/2018)
 ln -s notes/kafka-upgrade-notes $dest_dir
