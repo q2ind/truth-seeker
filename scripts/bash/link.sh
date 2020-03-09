5263c61e93a29aba6b636196fca40ea156e24f4b
Gary Chen <gary.chen@verizonmedia.com>
2020-03-09 10:34:28 -0400

    Updated link.sh

--- scripts/bash/link.sh
+++ scripts/bash/link.sh
@@ -3,8 +3,8 @@
 # Usage: $0 [day-shif]
 # Last updated: 2018-10-02
 #
-dest_dir=`pwd`
-echo "dest_dir=$dest_dir"
+dest_dir=$(pwd)
+#echo "dest_dir=$dest_dir"
 docs_dir=/Users/garychen/Documents/work/docs
 #echo $docs_dir
 jira_dir=/Users/garychen/Documents/work/jira
@@ -12,94 +12,101 @@ jira_dir=/Users/garychen/Documents/work/jira
 notes_dir=/Users/garychen/Documents/mystuff/notes
 
 # only link directory
-ln -s ${docs_dir} $dest_dir
-ln -s ${notes_dir} $dest_dir
-ln -s ${jira_dir} $dest_dir
+ln -s ${docs_dir} "$dest_dir"
+ln -s ${notes_dir} "$dest_dir"
+ln -s ${jira_dir} "$dest_dir"
 
 
 ## copy tasks and update start/end dates
 taskfile=$dest_dir/tasks
-/bin/cp $docs_dir/tasks-header $taskfile
+/bin/cp $docs_dir/tasks-header "$taskfile"
 
 # -- update dates in tasks
-# -- day_shift:
+# -- days_shift:
 #      positive number: shift forward
 #      negative number: shift backward
-#    e.g. run on 11/02 with day_shift=3, it will generate 11/05 - 11/09
-#         run on 11/02 with day_shift=-3, it will generate 10/30 - 11/03
+#    e.g. run on 11/02 with days_shift=3, it will generate 11/05 - 11/09
+#         run on 11/02 with days_shift=-3, it will generate 10/30 - 11/03
 #
-day_shift=0
+days_shift=0
 if [ "$#" -eq "1" ]; then
-    day_shift=$1
-    echo "day_shift=[$day_shift]"
+    days_shift=$1
+    echo "days_shift=[$days_shift]"
 fi
 
 days=(1 2 3 4 5)
 for day in "${days[@]}"; do
-    let "daydiff=$day-1+$day_shift"
-    echo "daydiff: ${daydiff}"
+    ##let "daydiff=$day-1+$days_shift"
+    (( daydiff=day-1+days_shift ))
+    #echo "daydiff: ${daydiff}"
     if (( daydiff < 0 )); then
-        formatted_day=`date -v ${daydiff}d '+%m/%d/%Y'`
+        formatted_day=$(date -v ${daydiff}d '+%m/%d/%Y')
     elif (( daydiff > 0 )); then
-        formatted_day=`date -v +${daydiff}d '+%m/%d/%Y'`
+        formatted_day=$(date -v +${daydiff}d '+%m/%d/%Y')
     else
-        formatted_day=`date '+%m/%d/%Y'`
+        formatted_day=$(date '+%m/%d/%Y')
     fi
-    echo "formatted_day = $formatted_day"
-    #formatted_day=`date -v +${daydiff}d '+%m/%d/%Y'`
+    #echo "formatted_day = $formatted_day"
+    #formatted_day=$(date -v +${daydiff}d '+%m/%d/%Y')
     #echo "day: ${day} = ${formatted_day}"
-    sed -i.bak "s%day${day}%$formatted_day%" $taskfile
+    sed -i.bak "s%day${day}%$formatted_day%" "$taskfile"
     if [ "$day" -eq "1" ]; then
-        sed -i.bak "s%week_start%$formatted_day%" $taskfile
+        sed -i.bak "s%week_start%$formatted_day%" "$taskfile"
     elif [ "$day" -eq "5" ]; then
-        sed -i.bak "s%week_end%$formatted_day%" $taskfile
+        sed -i.bak "s%week_end%$formatted_day%" "$taskfile"
     fi
 done
 ## clean up backup file
-if [ -f $taskfile.bak ]; then
-    /bin/rm -f $taskfile.bak
+if [ -f "$taskfile.bak" ]; then
+    /bin/rm -f "$taskfile".bak
 fi
 
-#appdir=reporting-validation
-
-#if [ -d $appdir ]; then
-#  echo "$appdir exists"
-#else
-#  echo "$appdir not existed"
-#fi
-
-#echo $?
-#grep 'test' data.sh
-#out=$?
-#echo $out
 
 ## link docs
-ln -s docs/myStuffToDo $dest_dir
-## link reporting-portal related tasks (as of 11/01/2018)
-#ln -s notes/reporting-b2b-upgrade $dest_dir
-## 
-## link kafka related notes (06/18/2018)
-ln -s notes/kafka-upgrade-notes $dest_dir
-ln -s notes/kafka_notes .
-ln -s notes/dev-kafka-notes .
-## link qiao notes (issues) 08/09/2018
-ln -s notes/qiao_notes .   
-#ln -s notes/kafka_load_tests_notes .
-ln -s notes/kafka-tasks $dest_dir
+ln -s docs/myStuffToDo .
+
+## link notes
+notes=(
+2019-goals
+dev-kafka-notes
+druid-tutorials-notes
+dulles-analytics
+kafka-loadtest-notes
+kafka-server-filter
+kafka-tasks
+kafka-upgrade-notes
+kafka_notes
+on-going-tasks
+separate-python-config-from-packer
+screwdriver-notes
+targetting-druid-notes
+yinst-notes
+)
+for note in "${notes[@]}"; do
+    ln -s notes/"$note" .
+done
+
+
+# link miscellaneous 
+# commented below 2 lines on 09/30/2019
+#ln -s ../jmx-metrics .
+#ln -s ../keystores .
 
 ## link previous week tasks
-let "days_diff=7-$day_shift"
-#let "days_diff=7+$day_shift"
-echo "-- debug -- days_diff=$days_diff"
-#prev_week=`date -v-7d +%m%d`
-prev_week=`date -v-${days_diff}d +%m%d`
-echo "-- debug -- prev_week=$prev_week"
-ln -s ../week-$prev_week/tasks prev.tasks
+if [ "$days_shift" -lt "7" ]; then
+    (( days_diff=7-days_shift ))
+    prev_week=$(date -v-"${days_diff}"d +%m%d)
+else
+    (( days_diff=days_shift-7 ))
+    prev_week=$(date -v+"${days_diff}"d +%m%d)
+fi
+
+ln -s ../week-"$prev_week"/tasks prev.tasks
 
 ## link DMP/CMP related notes
-ln -s notes/cmp_api_notes .
+## ln -s notes/cmp_api_notes .
 
-CURRENT_DIR=`pwd`
-echo $CURRENT_DIR
-
+CURRENT_DIR=$(pwd)
+echo "$CURRENT_DIR"
+ls -al
 
